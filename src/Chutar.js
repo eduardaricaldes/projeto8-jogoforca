01c1b2400ebcd19b751ac8c355513c63f5df797e
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-09-23 15:31:42 -0300

    criado componente para jogo da forca

---
+++ src/Chutar.js
@@ -0,0 +1,9 @@
+export default function Chutar(){
+  return(
+    <div className="chutar">
+      <input type="texto"></input>
+      <p>Já sei a palavra!</p>
+      <input type ="submit"></input>
+    </div>
+  )
+}
